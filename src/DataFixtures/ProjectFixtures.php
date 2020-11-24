a2ee7854047f87de3885027c5e4c3a18214909e9
dcaramello <dcaramello@gmail.com>
2020-11-24 15:40:36 +0100

    first commit

---
+++ src/DataFixtures/ProjectFixtures.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Project;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\DataFixtures\UserFixtures;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use App\Entity\User;
+use App\Entity\UserRepository;
+
+class ProjectFixtures extends Fixture implements DependentFixtureInterface
+{
+    public const ADMIN_PROJECT_REFERENCE = 'admin-project';
+
+    public function load(ObjectManager $manager)
+    {
+        // Sharing Objects between Fixtures
+        $user = $this->getReference(UserFixtures::ADMIN_USER_REFERENCE);
+
+        for($i = 1; $i <= 10; $i ++ ){
+            
+            $project = new Project();
+        
+        $project->setTitle("Projet n°$i")
+                ->setDescription("<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>")
+                ->setRegistered(new \DateTime())
+                ->setUserId($user)
+                ->setStatus("in progress");
+
+                $manager->persist($project);
+        }
+
+        $manager->flush();
+        $this->addReference(self::ADMIN_PROJECT_REFERENCE, $project);
+    }
+
+    public function getDependencies()
+    {
+        return array(
+            UserFixtures::class,
+        );
+    }
+}
---
+++ src/Form/ProjectType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Project;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProjectType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('registered')
+            ->add('deadline')
+            ->add('status')
+            ->add('user_id')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Project::class,
+        ]);
+    }
+}
