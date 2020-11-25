a25f172f605382b44cbf3bc98c5f14d13d5f1c9f
dcaramello <dcaramello@gmail.com>
2020-11-25 14:15:11 +0100

    login and register forms operationnal

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -9,14 +9,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+
+// pour proteger toutes les routes de l'application
 /**
- * @Route("/project")
+ * @IsGranted("IS_AUTHENTICATED_FULLY")
+ * @Route("/index")
  */
 class ProjectController extends AbstractController
 {
     /**
-     * @Route("/", name="project_index", methods={"GET"})
+     * @Route("/", name="/", methods={"GET"})
      */
     public function index(ProjectRepository $projectRepository): Response
     {
